44151adb310d510cecc4e9fc2fb851906f4ae2f6
Miao JingJing <MiaoJingJing@aactechnologies.com>
2022-09-20 17:39:09 +0800

    文件上传

--- Demo/upload_file/test_upload_file.py
+++ Demo/upload_file/test_upload_file.py
@@ -1,5 +1,6 @@
 import time
 
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 
 from base import Base
@@ -13,4 +14,24 @@ class Test_UPLoad_File(Base):
         time.sleep(3)
 
     def test_alert(self):
+        '''
+        1|打开网页：https://www.runoob.com/try/try.php?filename=jqueryui-api-droppable
+        操作窗口右侧的页面，将元素1拖拽到元素2
+        这时候会有一个alert弹框，点击确定
+        然后点击运行
+        关闭页面
+        '''
         self.driver.get("https://www.runoob.com/try/try.php?filename=jqueryui-api-droppable")
+        self.driver.switch_to.frame("iframeResult")
+        ele1=self.driver.find_element(By.ID,"draggable")
+        ele2=self.driver.find_element(By.ID,"droppable")
+        ActionChains(self.driver).drag_and_drop(ele1, ele2).perform()
+        time.sleep(3)
+        self.driver.switch_to.alert.accept()
+        print("点击确认")
+        time.sleep(3)
+
+        self.driver.switch_to.default_content()
+        self.driver.find_element(By.ID,"submitBTN").click()
+        time.sleep(3)
+
